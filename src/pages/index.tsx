6aab63547d29eed1c394ca6b8e7551bf2a121d41
Leg3ndre <leg3ndre@gmail.com>
2025-05-21 22:14:55 +0900

    HTMLの大枠を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,95 +1,37 @@
-import Image from 'next/image';
 import styles from './index.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+    <>
+      <h1 className={styles.title}>Second Person Shooting</h1>
+      <div className={styles.scores}>
+        <span id="pl-score" className={styles.plScore}>PLAYER&apos;S LIFE: 5</span>
+        <span id="en-score" className={styles.enScore}>ENEMY&apos;S LIFE: 5</span>
+      </div>
+      <canvas id="game" className={styles.gameCanvas} width="640" height="480"></canvas>
+      <a href="#">Retry</a>
+      <div className={styles.desc}>
+        <p className={styles.abstract}>
+          FPS（一人称視点）、TPS（三人称視点）に支配された現代のシューティングゲーム界隈に一石を投じる
+          <strong>二人称視点のシューティングゲーム</strong>
+          です。
+        </p>
+        <ul className={styles.playManual}>
+          <li>ゲームは<strong>敵視点</strong>で進行します</li>
+          <li>プレイヤーが操作するのは画面奥の黄色いキャラクター（Mr.K）です</li>
+          <li>三角形の物体が弾です（赤: 自分の弾、緑: 敵の弾）。弾は同時に5発までしか撃てません</li>
+          <li>敵の弾を避け、自分の弾を敵にうまく当ててください</li>
+          <li>※ 現状では敵のライフを0にしてもゲームは終了しません</li>
+          <li>操作方法
+            <ul>
+              <li>→ or D: 右へ移動</li>
+              <li>← or A: 左へ移動</li>
+              <li>↑ or W: 弾を発射</li>
+              <li>R: リトライ</li>
+            </ul>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </ul>
+      </div>
+    </>
   );
 }
