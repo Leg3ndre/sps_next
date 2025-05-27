b1883a5b6bbbe6c6ffc06ad6a823ad74ebe8b754
Leg3ndre <leg3ndre@gmail.com>
2025-05-27 23:47:21 +0900

    Rキーでリロード実装

--- src/components/retryButton/index.tsx
+++ src/components/retryButton/index.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const RetryButton = () => {
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key == 'r') location.reload();
+  }
+
   return (
     <a href="#" onClick={() => location.reload()}>Retry</a>
   );
